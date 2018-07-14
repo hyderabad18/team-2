4bb43a16552a072eecdc0bc528d8aa0560e26041
rohith reddy <rohithreddy019@gmail.com>
2018-07-15 02:04:53 +0530

    adding java file

---
+++ AdminEDVolunteers.java
@@ -0,0 +1,13 @@
+package com.example.rohith.firebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AdminEDVolunteers extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_edvolunteers);
+    }
+}
