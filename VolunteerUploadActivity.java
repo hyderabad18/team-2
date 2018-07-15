3d9c2d15465b51be118650ac4ab41c941f6263c5
rohith reddy <rohithreddy019@gmail.com>
2018-07-15 10:35:21 +0530

    ADDING upload file

---
+++ VolunteerUploadActivity.java
@@ -0,0 +1,14 @@
+package com.example.rohith.firebase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class VolunteerUploadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_volunteer_upload);
+    }
+
+}
