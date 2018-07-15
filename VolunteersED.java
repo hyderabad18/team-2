56765d8ddf1c07f7ac82116fb3062be3ce15e269
rohith reddy <rohithreddy019@gmail.com>
2018-07-15 10:34:30 +0530

    adding volunteers data

---
+++ VolunteersED.java
@@ -0,0 +1,22 @@
+package com.example.rohith.firebase;
+
+public class VolunteersED {
+    private String name;
+    private String skill;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+}
